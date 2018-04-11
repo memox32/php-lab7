83f27dae2db28e724259349670a276ad991fd082
Guillermo Romero <38238157+memox32@users.noreply.github.com>
2018-04-11 19:22:37 +1200

    Update lab7task_login_mid.php

--- lab7task_login_mid.php
+++ lab7task_login_mid.php
@@ -5,10 +5,28 @@
  * Date: 10/04/2018
  * Time: 10:16 AM
  */
-include "lab6Task1.php";
-session_start();
-$name = $age = $gender = $tel = $email = $income = $comments = $image = $tax= $total = $nameErr = "";
 
+session_start();
+
+//include ($_SERVER['DOCUMENT_ROOT'].'lab6Task1.php');
+include "lab6Task1.php";
+//include ("C:\inetpub\wwwroot\lab6Task1.php");
+
+//set_include_path(get_include_path().PATH_SEPARATOR.'lab6Task1.php');
+
+
+$name = $age = $gender = $tel = $email = $income = $comments = $image = $tax= $total = "";
+
+$nameErr = $ageErr = $genderErr = $telErr = $incomeErr = $commentsErr = $imgErr = $totalErr = "";
+
+formValidationText("name",$name,$nameErr);
+formValidationNumber("age",$name,$nameErr);///numbers
+formValidationText("gender",$name,$nameErr);
+formValidationNumber("tel",$name,$nameErr);///numbers
+formValidationText("email",$name,$nameErr);////email
+formValidationNumber("income",$name,$nameErr);////numbers
+formValidationText("comments",$name,$nameErr);
+formValidationText("image",$name,$nameErr);///image
 
 $name = $_POST["name"];
 $age = $_POST["age"];
