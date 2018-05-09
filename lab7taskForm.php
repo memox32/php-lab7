eda01291decb7901c8c4c3bd22e2f055505811fc
Guillermo Romero <38238157+memox32@users.noreply.github.com>
2018-05-09 23:09:48 +1200

    Final version

--- lab7taskForm.php
+++ lab7taskForm.php
@@ -1,38 +1,44 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: TOSHIBA
+ * Created by Memo.
  * Date: 10/04/2018
- * Time: 10:16 AM
  */
 
-//session_start();
-//$valid = false;
-//include ($_SERVER['DOCUMENT_ROOT'].'lab6Task1.php');
-include "functions.php";
+//Include the functions file to implements the functions created
+include "functionsGitLab.php";
 
+//Declaration of variables
 $name = $age = $gender = $tel = $email = $income = $comments = $image = $tax= $total = "";
-
 $nameErr = $ageErr = $genderErr = $telErr = $emailErr = $incomeErr = $commentsErr = $imgErr = $totalErr = "";
 
+//Functions to validate the different input values in the HTML form
 formValidationText("name",$name,$nameErr);
-formValidationNumber("age",$age,$ageErr);///numbers
-formValidationText("gender",$gender,$genderErr);////radio
-formValidationNumber("tel",$tel,$telErr);///numbers
-formValidationEmail("email",$email,$emailErr);////email
-formValidationNumber("income",$income,$incomeErr);////numbers
+formValidationNumber("age",$age,$ageErr);
+formValidationText("gender",$gender,$genderErr);
+formValidationNumber("tel",$tel,$telErr);
+formValidationEmail("email",$email,$emailErr);
+formValidationNumber("income",$income,$incomeErr);
 formValidationText("comments",$comments,$commentsErr);
-formValidationText("image",$image,$imgErr);///image
+formValidationText("image",$image,$imgErr);///image feature will be include later
 
 ?>
 
-<form action="<?php echo  htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype = "multipart/form-data" target="_self">
+<!--Form to get the values from the user-->
+<!--htmlspecialchars is used to prevent XSS, this method is preferred instead of action = "" since this might also lead to security problems "-->
+<form action="<?php echo  htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype = "multipart/form-data">
+<!--Declaration of a flag that will be used to send the form after the validation is correct-->
     <?php  $valid = true?>
+<!--  Style to apply to the error messages  -->
     <style type="text/css"> .error{color:red;}</style>
+    <!--  Style to apply to the asterisk  -->
     Please enter the following information:<br>
     <span style = 'color : red'> Required fields *</span><br><br>
+<!-- value = " php echo $name used to keep the values after the validation failed and more fields need to be re-entered   -->
     Name: <input type="text" name = "name" value = "<?php echo $name;?>">
     <span class="error">*</span>
+<!--  This combination of a ternary operator inside a if statement is used for displaying the error message in case a field has no information
+  or has incorrect information, the ternary operator checks if an error was thrown from the functions above.
+  If any error was triggered the if statement set the flag to false to the form is refreshed and asking the user to enter or modify the input-->
     <?php  if(($nameErr == 1 ? trigger_error("Name is required") : ""))$valid = false; ?>
     <?php if(($nameErr == 2 ? trigger_error("Only letters and white space allowed") : ""))$valid = false;?><br><br>
     Age: <input type="text" name="age" value = "<?php echo $age;?>">
@@ -55,32 +61,22 @@ formValidationText("image",$image,$imgErr);///image
     Income: <input type="text" name="income" value = "<?php echo $income;?>">
     <span class="error">*</span>
     <?php  if(($incomeErr == 1 ? trigger_error("Income is required") : ""))$valid = false;?>
-    <?php if(($incomeErr == 2 ? trigger_error("Only numbers allowed") : ""))$valid = false;?><br><br>
+    <?php if(($incomeErr == 2 ? trigger_error("Only numbers allowed") : ""))$valid = false;?>
+    <?php if(($incomeErr == 3 ? trigger_error("Only positive numbers allowed") : ""))$valid = false;?><br><br>
     Comments: <textarea rows="4" cols="50" name="comments"><?php echo $comments;?></textarea>
     <br><br>
-    Upload image: <input type="file" name="img"><br><br>
+    Upload image: <input type="file" name="imgToUpload"><br><br>
     <input type="submit" value = "Calculate tax" name="submit">
+<!--  This is necessary otherwise when the form is loaded for the first time, the if statement to validate the flag that is written below includes the login_mid.php so this ensure the user clicks the submit button and a validation takes places before moving to the login_mid.php file -->
     <?php if ($_SERVER['REQUEST_METHOD'] != 'POST'){$valid = false;} ?>
     </form>
 
 <?php
-//<?php $valid = false?
 
-if(!$valid){
-    echo "lili";
+if($valid){
+    //header("location:lab7task_login_mid.php");
+    //include login_mid.php so it can store the cookies and session info, if you use header instead the info will not be saved.
+    include "lab7task_login_mid.php";
 }
-else{
-    echo "lulu";
-    header("location:lab7task_login_mid.php");
-}
-echo $email;
-//if($nameErr ="1" || $nameErr ="2"){
-//
-//    echo "lala";
-//    //header("location:lab7task_login_display.php");
-//
-//}//}
-//else{
-//    echo "lollo";
-//}
+
 ?>
