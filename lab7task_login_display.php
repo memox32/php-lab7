f9e3acf2ba64591b1e44ad7b94d90e42cfc1245c
Guillermo Romero <38238157+memox32@users.noreply.github.com>
2018-05-09 23:03:43 +1200

    Update lab7task_login_display.php

--- lab7task_login_display.php
+++ lab7task_login_display.php
@@ -2,16 +2,14 @@
 /**
  * Created by Memo.
  * Date: 10/04/2018
- * Time: 10:16 AM
  */
 
 //Starting the session
 session_start();
 
 //Getting value sent in the header at the end of the login_mid file
-$total = $_GET['total'];
 
-$total = number_format($total,2);
+$total = $_COOKIE["income"]; //. "<br>"
 
 //Display the information using cookies and sessions
 echo "Your gross income is $" . $total . "<br>";
@@ -21,7 +19,7 @@ echo "Name: " . $_SESSION["name"] . "<br>";
 echo "Age: " . $_SESSION["age"]. "<br>";
 echo "Gender: " . $_COOKIE["gender"]. "<br>";
 echo "Tel: " . $_COOKIE["tel"]. "<br>";
-echo "Email: " . $_COOKIE["income"]. "<br>"; //If you use () you get: Fatal error: Function name must be a string
+echo "Email: " . $_COOKIE["email"]. "<br>";
 echo "Comments: " . $_SESSION["comments"]. "<br>";
 
 ?>
