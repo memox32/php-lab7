feed34b3734c0bf0426a69d841607c1d088c1f71
Guillermo Romero <38238157+memox32@users.noreply.github.com>
2018-05-09 23:07:18 +1200

    Final version

--- lab7task_login_mid.php
+++ lab7task_login_mid.php
@@ -1,54 +1,43 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: TOSHIBA
+ * Created by Memo.
  * Date: 10/04/2018
- * Time: 10:16 AM
  */
 
+//Starting the session
 session_start();
 
-//include ($_SERVER['DOCUMENT_ROOT'].'lab6Task1.php');
-include "lab6Task1.php";
-//include ("C:\inetpub\wwwroot\lab6Task1.php");
-
-//set_include_path(get_include_path().PATH_SEPARATOR.'lab6Task1.php');
-
-
+//Declaration of variables
 $name = $age = $gender = $tel = $email = $income = $comments = $image = $tax= $total = "";
-
-$nameErr = $ageErr = $genderErr = $telErr = $incomeErr = $commentsErr = $imgErr = $totalErr = "";
-
-formValidationText("name",$name,$nameErr);
-formValidationNumber("age",$name,$nameErr);///numbers
-formValidationText("gender",$name,$nameErr);
-formValidationNumber("tel",$name,$nameErr);///numbers
-formValidationText("email",$name,$nameErr);////email
-formValidationNumber("income",$name,$nameErr);////numbers
-formValidationText("comments",$name,$nameErr);
-formValidationText("image",$name,$nameErr);///image
-
-$name = $_POST["name"];
-$age = $_POST["age"];
-$gender = $_POST["gender"];
-$tel = $_POST["tel"];
-$email = $_POST["email"];
-$income = $_POST["income"];
-$comments = $_POST["comments"];
-$image = $_POST["image"];
+$nameErr = $ageErr = $genderErr = $telErr = $emailErr = $incomeErr = $commentsErr = $imgErr = $totalErr = "";
+$target_dir = "upload\\";
 $tax= .16;
-//$total ="";
 
+//Function for set cookies and session variables
 function setCustomInfo(){
 
-    $SESSION["name"] = "meme";
-    $SESSION["age"] = "28";
-    setcookie("gender",  "male");
-    setcookie("tel", 3030);
-    setcookie("income", $_POST["income"]);
-    $SESSION["comments"] = $comments = $_POST["comments"];
+    $_SESSION["name"] = $_POST["name"];
+    $_SESSION["age"] = $_POST["age"];
+    setcookie("gender",  $_POST["gender"]);
+    setcookie("tel", $_POST["tel"]);
+    setcookie("email", $_POST["email"]);
+    //setcookie("income", $_POST["income"]);
+    $_SESSION["comments"] = $_POST["comments"];
 }
 
-($income)> 0 ? $total = ($income * $tax) - $income: "Write the income";
+setCustomInfo();
 
-header("location:lab7task_login_display.php?total=".urlencode($total));
+if(!empty($_POST["income"])){
+    $income = $_POST["income"];
+}
+
+//In this case is better to set the cookie after getting the post value
+$total = $income - ($income * $tax);
+$total = number_format($total,2);
+setcookie("income", $total);
+
+if($_POST['submit']){
+//This approach can be used to pass the value in the header, for instance if the cookie "income" is not set
+//    header("location:lab7task_login_display.php?total=".urlencode($total));
+    header("location:lab7task_login_display.php");
+}
